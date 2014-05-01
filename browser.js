b3562c2798507869edb767da869cd7b85487726d
Nathan Rajlich <nathan@tootallnate.net>
2014-05-01 10:42:38 -0700

    browser: more jsdocs

--- browser.js
+++ browser.js
@@ -8,7 +8,14 @@ module.exports = deprecate;
 /**
  * Mark that a method should not be used.
  * Returns a modified function which warns once by default.
- * If --no-deprecation is set, then it is a no-op.
+ *
+ * If `localStorage.noDeprecation = true` is set, then it is a no-op.
+ *
+ * If `localStorage.throwDeprecation = true` is set, then deprecated functions
+ * will throw an Error when invoked.
+ *
+ * If `localStorage.traceDeprecation = true` is set, then deprecated functions
+ * will invoke `console.trace()` instead of `console.error()`.
  *
  * @param {Function} fn - the function to deprecate
  * @param {String} msg - the string to print to the console when `fn` is invoked
